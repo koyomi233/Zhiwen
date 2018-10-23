cb5a829ae636ede8b73c0dff4ace53906eb85d18
Zhiwen Han <317657452@qq.com>
2018-10-23 10:34:09 +0100

    Get collections by name end point tested

--- models/collection.js
+++ models/collection.js
@@ -3,8 +3,8 @@ let mongoose = require('mongoose');
 let CollectionSchema = new mongoose.Schema({
         category: String,
         name: String,
-        size: {Types: Number, default: 0},
-        follow: {Types: Number, default: 0}
+        size: Number,
+        follow: Number
     },
     { collection: 'collections' });
 
--- test/routes/collection-test.js
+++ test/routes/collection-test.js
@@ -35,9 +35,17 @@ describe('Collection', () => {
             follow: 12
         });
 
+        var collection4 = new datastore({
+            category: 'UI/UX',
+            name: 'Illustration',
+            size: 0,
+            follow: 0
+        })
+
         collection1.save();
         collection2.save();
         collection3.save();
+        collection4.save();
     });
     describe('GET /collection', () => {
         it('should return all the collections in an array', function (done) {
@@ -53,6 +61,33 @@ describe('Collection', () => {
                     expect(result).to.include( { category: 'Animation', name: 'Japanese illustration'  } );
                     expect(result).to.include( { category: 'sketch', name: 'Black and White'  } );
                     expect(result).to.include( { category: 'photography', name: 'Girls!'  } );
+                    done();
+                });
+        });
+    });
+    describe('GET /collection/names/:name', () => {
+        it('should return several collections which matches the name', function (done) {
+            chai.request(server)
+                .get('/collection/names/illustration')
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(2);
+                    let  result = _.map(res.body, (board) => {
+                        return { category: board.category,
+                            name: board.name }
+                    });
+                    expect(result).to.include( { category: 'Animation', name: 'Japanese illustration'  } );
+                    expect(result).to.include( { category: 'UI/UX', name: 'Illustration'  } );
+                    done();
+                });
+        });
+        it('should return a message for invalid collection name ', function (done) {
+            chai.request(server)
+                .get('/collection/names/Marvel')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message', 'Collection NOT Found!' ) ;
                     done();
                 });
         });

197a17e6c99de9e10de3100cb594a57658f71ea9
Zhiwen Han <317657452@qq.com>
2018-10-25 13:47:01 +0100

    Get all users endpoint tested

---
+++ test/routes/user-test.js
@@ -0,0 +1,38 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../../bin/www');
+let expect = chai.expect;
+var Collection = require('../../models/collection');
+var Picture = require('../../models/picture');
+var User = require('../../models/user');
+
+chai.use(chaiHttp);
+chai.use(require('chai-things'));
+let _ = require('lodash' );
+
+require('./db_globle');
+
+describe('User', () => {
+    describe('GET /user', () => {
+        it('should return all the users in an array', function (done) {
+            chai.request(server)
+                .get('/user')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(3);
+                    let result = _.map(res.body, (account) => {
+                        return { name: account.name,
+                            email: account.email}
+                    });
+                    expect(result).to.include( { name: 'soundtrack', email: '317657452@qq.com'  } );
+                    expect(result).to.include( { name: 'koyomi', email: '317657452h@sina.com'  } );
+                    expect(result).to.include( { name: 'Shinobu', email: '317657452h@126.com'  } );
+                    Collection.collection.drop();
+                    Picture.collection.drop();
+                    User.collection.drop();
+                    done();
+                });
+        });
+    });
+})
